65be46b6ebc565ec6a4ba6e931ef6ede8a0f5c37
Christian Tsoi-A-Sue <ctsoi@stanford.edu>
2026-05-08 13:01:15 -0700

    continues cleanup and docstrings

--- lcls_user_motor_gui/user_motor_gui.py
+++ lcls_user_motor_gui/user_motor_gui.py
@@ -1,10 +1,6 @@
-import asyncio
-import json
 import logging
 import re
 import sys
-import time
-from enum import Enum
 from os import path
 from pathlib import Path
 
@@ -13,38 +9,16 @@ import epics
 # import epics
 from epics import PV, caget, caput
 from pcdsutils.qt.designer_display import DesignerDisplay
-
-# from epics import PV, fake_caget, cainfo, caput
-from pydm import Display
-from pydm.widgets.enum_combo_box import PyDMEnumComboBox
 from pydm.widgets.label import PyDMLabel
 from pydm.widgets.line_edit import PyDMLineEdit
-from pydm.widgets.pushbutton import PyDMPushButton
-from PyQt5.QtCore import pyqtSignal
-from PyQt5.QtWidgets import QCompleter, QLineEdit, QListView, QVBoxLayout, QWidget
-from qtpy import QtCore, uic
-from qtpy.QtCore import Qt
+from PyQt5.QtWidgets import QWidget
 from qtpy.QtWidgets import (
-    QAbstractItemView,
     QApplication,
-    QCheckBox,
-    QComboBox,
-    QCompleter,
     QDialog,
-    QFrame,
-    QGroupBox,
-    QHBoxLayout,
-    QLabel,
-    QLineEdit,
-    QListView,
     QListWidget,
-    QListWidgetItem,
-    QMessageBox,
     QPlainTextEdit,
     QPushButton,
-    QScrollArea,
     QTabWidget,
-    QVBoxLayout,
     QWidget,
 )
 from qtpy.uic import loadUi
@@ -175,72 +149,41 @@ class MainWindow(DesignerDisplay, QWidget):
         self.setting_widget = SettingsWindow()
         self.main_tabs.addTab(self.setting_widget, "Settings")
 
-        # Mapping message box
-        self.msg = QMessageBox()
-        self.isMsgActive = False
-
         # initialize vars
-        init = True
-        if init:
-            init = False
-            self.prefixName = ""
-            self.testList = []
-            self.loadedList = []
-            self.pvDict = {}
-            self.pvDict_table = {}
-            self.expertDict = {}
-            self.plc_ioc_list = []
-            self.plc_ioc_label = ""
-            self.axis = []
-            self.drives = []
-            self.digital_inputs = ["None"]
-            # self.digital_inputs_ui = ["None"]
-            self.digital_inputs_hardware = ["None"]
-            self.digital_inputs_hardware_ui = ["None"]
-            self.drives_linker = ["None"]
-            self.encoders = ["None"]
-            self.enocders_list = []
-            self.list_WCIB = []
-            self.cleaned_di = ""
-            self.di_num_channels = 0
-            self.loaded_unique_di = []
-            # self.loaded_unique_di_ui = []
-            # self.loaded_di_channels = []
-            # self.loaded_di_channels_ui = []
-            self.loaded_di_channel_inputs = []
-            self.store_di_selection = [[-1, -1], [-1, -1], [-1, -1]]
-            self.axis_di_idx = 0
-            self.axis_di_init = True
-            self.di_size = 0
-            # self.staged_mapping = []
-            # self.staged_de = []
-            # self.qCurrAxis = 0
-            self.ncList = []
-            self.coeList = []
-            self.wcibList = []
-            self.wcibDict = {}
+        self.prefixName = ""
+        self.pvDict = {}
+        self.axis = []
+        self.drives = []
+        self.digital_inputs = ["None"]
+        self.drives_linker = ["None"]
+        self.encoders = ["None"]
+        self.list_WCIB = []
+        self.ncList = []
+        self.coeList = []
+        self.wcibList = []
+        self.wcibDict = {}
 
-            # self.dg_list = []
-            self.ca_nc_list = []
-            self.ca_coe_drive_list = []
-            self.ca_coe_encoder_list = []
-            self.ca_dg_list = []
-            self.param_connections = []
-            self.ioc_path = "/reg/g/pcds/epics-dev/nlentz/lcls-plc-template-user-motors/iocBoot/ioc-lcls-plc-template-user-motors/lcls_plc_template_user_motors.db"
+        # self.dg_list = []
+        self.ca_nc_list = []
+        self.ca_coe_drive_list = []
+        self.ca_coe_encoder_list = []
+        self.ca_dg_list = []
+        self.param_connections = []
+        self.ioc_path = "/reg/g/pcds/epics-dev/nlentz/lcls-plc-template-user-motors/iocBoot/ioc-lcls-plc-template-user-motors/lcls_plc_template_user_motors.db"
 
         self.start_gui()
 
     def start_gui(self):
-        logger.info(f"in start_gui")
         """
         Load IOC pvs from ioc db, setup the tab signals and populate data from loaded db
         """
+        logger.info(f"in start_gui")
+
         self.load_ioc_data()
         self.setup_tab_signals()
         self.populate_options()
 
     def setup_tab_signals(self):
-        logger.info(f"in setup_tab_signals")
         """
         Setup all of the signals for each of the tab widgets.
         """
@@ -248,6 +191,8 @@ class MainWindow(DesignerDisplay, QWidget):
         """
         Settings tab
         """
+
+        logger.info(f"in setup_tab_signals")
         # self.setting_widget.settings_duplicate_di_warning.stateChanged.connect(
         #     self.check_duplicate_di_flag
         # )
@@ -376,14 +321,6 @@ class MainWindow(DesignerDisplay, QWidget):
 
     #     logger.debug(f"isDuplicateDIWarning: {self.duplicate_enc_cb_flag}")
 
-    def extract_unique_parts(self, pv_names):
-        unique_parts = set()
-        for pv in pv_names:
-            parts = pv.split(":")
-            if len(parts) > 4:
-                unique_parts.add(parts[4])
-        return sorted(unique_parts)
-
     # Currently not used
     def when_param_changed(self, idx, pv, lineedit):
         logger.debug(f"in when_param_changed")
@@ -438,38 +375,8 @@ class MainWindow(DesignerDisplay, QWidget):
         """
 
         logger.info(f"in load test list")
-        configured = "./unit_test_data.json"
-        integration_test = "/cds/home/c/ctsoi/epics-dev/ioc/user_motors/lcls-plc-template-user-motors/iocBoot/ioc-lcls-plc-template-user-motors/lcls_plc_template_user_motors.db"
-        integration_box_test = "/reg/g/pcds/epics-dev/nlentz/lcls-plc-template-user-motors/iocBoot/ioc-lcls-plc-template-user-motors/lcls_plc_template_user_motors.db"
 
-        # ## test with configuration
-        # iocpath = configured
-
-        # try:
-        #     # with open(f"{filepath}", "r") as f:
-        #     #     for pvs in f:
-        #     #         pv_caget_list.append(pvs)
-
-        #     with open(iocpath, "r") as file:
-        #         self.pvDict = json.load(file)
-        # except Exception as e:
-        #     logger.debug(f"Failed to read {filepath1}: {e}")
-
-        # self.prefixName = list(self.pvDict.keys())[0]
-        # pattern = r"(TST:UM:)(?=\S)"
-
-        # match = re.search(pattern, self.prefixName)
-        # if match:
-        #     logger.debug(match.group(1))  # Output: TST:UM:
-        # self.prefixName = match.group(1)
-
-        ## integration test
-        iocpath = integration_box_test
-
-        # hard code ioc path
-        # self.pvList = discover_pvs("", usr_db_path=iocpath, find_makefile=True)
-
-        # find using ioc name
+        # find using ioc name, discover pvs has other options for find the ioc information
         self.pvList = discover_pvs(self.ioc_name, plc_flag=True, find_makefile=True)
 
         # for testing only
@@ -522,13 +429,12 @@ class MainWindow(DesignerDisplay, QWidget):
         # logger.debug(self.pvDict)
 
     def populate_options(self):
-        logger.info(f"in populate options")
-
         """
         Called from load_ioc
         ---
         Calls WCIB
         """
+        logger.info(f"in populate options")
         # identify WCIB PVs
         self.identify_WCIB()
 
@@ -601,68 +507,15 @@ class MainWindow(DesignerDisplay, QWidget):
         self.user_input_widget.load_encoders_ui()
 
     def clear_items(self):
+        """
+        Clears the WCIB list, both digitial input lists in ui/linker and the drives and encoders in linker
+        """
+        logger.info("in clear_items")
         self.list_WCIB.clear()
         self.digital_inputs.clear()
         self.user_input_widget.digital_inputs_ui.clear()
         self.drives_linker.clear()
         self.encoders.clear()
-
-    def publish_axis_di(self):
-        logger.info(f"in publish_axis_di")
-        # if self.axis_di_init:
-        self.linker_widget.digital_input_axis.clear()
-        numDI = 0
-
-        currAxis = self.val_to_key(
-            self.linker_widget.axis_list_linker.currentItem().text()
-        )
-        logger.debug(f"currAxis: {currAxis}")
-
-        # need to change this to number of DIs
-        for i in range(0, 3):
-            self.linker_widget.digital_input_axis.addItem("0" + str(1 + i))
-
-        self.select_di_channel()
-
-    def publish_axis(self):
-        """
-        Called from load_axis
-        ---
-
-        """
-        # update enum with axis pulled from .db file
-        logger.info(f"in populate axis")
-        self.linker_widget.axis_list_linker.clear()
-
-        # for item in self.axis:
-        #     self.axis_list.addItem(item)
-
-        self.linker_widget.axis_list_linker.addItems(self.axis)
-
-        if not self.linker_widget.axis_list_linker.isEnabled():
-            self.linker_widget.axis_list_linker.setEnabled(True)
-
-        self.linker_widget.staged_mapping = [[["01"], ["02"], ["03"]]]
-        self.linker_widget.staged_de = [[["None"], ["None"]]]
-
-    def load_axis_di(self):
-        """ """
-        logger.info(f"in load_axis_di")
-        self.linker_widget.digital_input_axis.clear()
-
-        # logger.debug(f"di_val: {axis_di}")
-        for item in self.axis:
-            logger.debug(f"axis: {item}")
-            self.loaded_unique_di.append(self.identify_di(item))
-
-            # self.digital_input_axis.addItem(val)
-        self.loaded_unique_di = [
-            item for sublist in self.loaded_unique_di for item in sublist
-        ]
-        logger.debug(f"val: {self.loaded_unique_di}")
-        # if not self.digital_input_axis.isEnabled():
-        #     self.digital_input_hardware.setEnabled(True)
-        # self.discover_di_channel()
 
 
 if __name__ == "__main__":
