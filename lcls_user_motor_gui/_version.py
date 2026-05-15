5b8d70811aa0bc5aa26d006f5d8e663571e00664
Christian Tsoi-A-Sue <ctsoi@stanford.edu>
2026-05-15 15:27:50 -0700

    updating version

--- lcls_user_motor_gui/_version.py
+++ lcls_user_motor_gui/_version.py
@@ -18,7 +18,7 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = '0.1.dev66+g9906b62dd.d20260515'
-__version_tuple__ = version_tuple = (0, 1, 'dev66', 'g9906b62dd.d20260515')
+__version__ = version = '0.1.dev73+g1b7b7de11.d20260515'
+__version_tuple__ = version_tuple = (0, 1, 'dev73', 'g1b7b7de11.d20260515')
 
-__commit_id__ = commit_id = 'g9906b62dd'
+__commit_id__ = commit_id = 'g1b7b7de11'
