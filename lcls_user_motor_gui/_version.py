447ad06ab295989f173f6540b740fc973dd8adb1
Christian Tsoi-A-Sue <ctsoi@stanford.edu>
2026-05-15 08:50:57 -0700

    attempting to fix some of the github run information

--- lcls_user_motor_gui/_version.py
+++ lcls_user_motor_gui/_version.py
@@ -18,7 +18,7 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = "0.1.dev63+g5f618b873.d20260512"
-__version_tuple__ = version_tuple = (0, 1, "dev63", "g5f618b873.d20260512")
+__version__ = version = '0.1.dev66+g9906b62dd.d20260515'
+__version_tuple__ = version_tuple = (0, 1, 'dev66', 'g9906b62dd.d20260515')
 
-__commit_id__ = commit_id = "g5f618b873"
+__commit_id__ = commit_id = 'g9906b62dd'
